b43575879d6b2afffde788548254246dac0c7e4f
Sarah Stephens <sarahstephens84@gmail.com>
2022-08-23 15:44:51 -0400

    commit tree 1e29f9fc7bf885f46e7582941c0986fe19fc9324

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(cat){
+    cats.push(cat);
+}
+function destructivelyPrependCat(cat){
+    cats.unshift(cat);
+}
+function destructivelyRemoveLastCat(cat){
+    cats.pop(cat);
+}
+function destructivelyRemoveFirstCat(cat){
+    cats.shift(cat);
+}
+
+function appendCat(cat){
+    appendCat = [...cats,cat]
+    return appendCat;
+}
+
+function prependCat(cat){
+    prependCat = [cat,...cats]
+    return prependCat;
+}
+
+function removeLastCat(){
+    removeLastCat = cats.slice(0,-1);
+    return removeLastCat;
+}
+
+function removeFirstCat(){
+    removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
